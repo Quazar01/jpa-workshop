68e113143f8cc1d883a65a96fab921940fd70be1
Sami Alabed <sami.alabed.94@gmail.com>
2024-09-19 10:52:06 +0200

    First part, and a little more.

---
+++ src/main/java/se/lexicon/jpaworkshop/entity/AppUser.java
@@ -0,0 +1,43 @@
+package se.lexicon.jpaworkshop.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDate;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
+
+@Entity
+public class AppUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Setter
+    @Column(nullable = false, unique = true, length = 20)
+    private String uesrname;
+
+    @Setter
+    @Column(nullable = false, length = 100)
+    private String password;
+
+    @Setter
+    @Column(nullable = false, length = 6)
+    LocalDate regDate;
+
+    @Setter
+    @OneToOne
+    @JoinColumn(name = "details_id")
+    private Details details;
+
+    // Constructor
+    public AppUser(String uesrname, String password) {
+        this.uesrname = uesrname;
+        this.password = password;
+        this.regDate = LocalDate.now();
+    }
+}
